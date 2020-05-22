b6576f132a3e2fc714469ab52aabc53f22137904
dejvid98 <davidsucur111@gmail.com>
2020-05-22 19:49:17 +0200

    Plating, Harvesting...

--- controllers/SeedlingController.js
+++ controllers/SeedlingController.js
@@ -25,26 +25,106 @@ exports.createSeedling = async (req, res) => {
   }
 };
 
-exports.plantSeedling = async (req, res) => {
+exports.getAvailableUserSeedlings = async (req, res) => {
   try {
     const {id} = req.body;
+
+    const seedlingsQuery = `SELECT * FROM seedling 
+                          WHERE id = $1 AND is_planted=false`;
+
+    const result = await db.query(seedlingsQuery, [id]);
+
+    res.send({
+      status: true,
+      data: result.rows,
+    });
+  } catch (err) {
+    res.send({status: false, message: err.message});
+  }
+};
+
+exports.plantSeedling = async (req, res) => {
+  try {
+    const {id, nurseryid} = req.body;
 
     const plantedDate = moment();
 
     const harvestDate = moment().add(20, 'days');
 
+    const checkIfPlantedQuery = `SELECT * FROM seedling 
+                                 WHERE id = $1`;
+
+    const seedResult = await db.query(checkIfPlantedQuery, [id]);
+
+    const seedDeconcstured = {...seedResult.rows[0]};
+
+    const isPlanted = seedDeconcstured.is_planted;
+
+    if (isPlanted) {
+      res.send({status: false, message: 'Seedling already planted!'});
+      return;
+    }
+
+    const updateAvailableSpots = `UPDATE nursery
+                                  SET available_space = available_space - 1
+                                  WHERE id = $1`;
+
+    await db.query(updateAvailableSpots, [nurseryid]);
+
     const seedlingQuery = `UPDATE seedling
                            SET is_planted = $1,
                                planted_date = $2,
-                               harvest_date = $3
-                           WHERE id = $4`;
+                               harvest_date = $3,
+                               nursery_id = $4
+                           WHERE id = $5`;
 
-    await db.query(seedlingQuery, [true, plantedDate, harvestDate, id]);
+    await db.query(seedlingQuery, [
+      true,
+      plantedDate,
+      harvestDate,
+      nurseryid,
+      id,
+    ]);
 
     res.send({
       status: true,
       message: 'Seedling successfully planted!',
     });
+  } catch (err) {
+    res.send({
+      status: false,
+      message: err.message,
+    });
+  }
+};
+
+exports.harvestSeedling = async (req, res) => {
+  try {
+    const {id, nurseryid} = req.body;
+
+    const seedlingQuery = 'SELECT * FROM seedling WHERE id = $1';
+
+    const result = await db.query(seedlingQuery, [id]);
+
+    const resultDeconstructed = {...result.rows[0]};
+
+    if (resultDeconstructed.harvest_date >= moment()) {
+      res.send({
+        status: false,
+        message: 'Seedling is not ready for harvesty yet!',
+      });
+      return;
+    }
+
+    const harvestQuery = 'DELETE FROM seedling WHERE id = $1';
+
+    await db.query(harvestQuery, [id]);
+
+    const nurseryQuery = `UPDATE nursery 
+                         SET available_spots = available_spots + 1 
+                         WHERE id = $1`;
+
+    await db.query(nurseryQuery, [nurseryid]);
   } catch (err) {
     res.send({
       status: false,
--- routes/SeedlingRoute.js
+++ routes/SeedlingRoute.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const {
   createSeedling,
   plantSeedling,
+  getAvailableUserSeedlings,
+  harvestSeedling,
 } = require('../controllers/SeedlingController');
 
 router.route('/add').post(createSeedling);
 
 router.route('/plant').post(plantSeedling);
+
+router.route('/').get(getAvailableUserSeedlings);
+
+router.route('/harvest').post(harvestSeedling);
 
 module.exports = router;
